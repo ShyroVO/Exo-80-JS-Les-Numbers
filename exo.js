526ab53b3746b82ebc5ad9aa21baf2934727011f
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-15 21:48:56 +0100

    1.0

--- exo.js
+++ exo.js
@@ -1,27 +1,41 @@
+// Exo 1
 let integer = document.getElementById('is-integer');
-let intergerNumber = parseInt(integer);
 
-
-if (Number.isInteger(intergerNumber)) {
+if (Number.isInteger(parseFloat(integer.innerHTML))) {
     integer.innerHTML += " ==> est un nombre entier.";
 }
 else {
     integer.innerHTML += " ==> n'est pas un nombre entier.";
 }
 
-
+// Exo 2
 let nan = document.getElementById('is-nan');
 
-if (isNaN(nan)) {
+if (isNaN(nan.innerHTML)) {
     nan.innerHTML += " ==> est NaN ( Not a Number )";
 }
 else {
     nan.innerHTML += " ==> est un nombre.";
 }
 
+// Exo 3
+let float = document.getElementById('parse-float');
+let parseFloatFloat = parseFloat(float.innerHTML);
+let parseFloatMulti = parseFloatFloat * 21;
+
+float.innerHTML.replace(parseFloatFloat.value, parseFloatMulti.value);
+
+// Exo 4
+let int = document.getElementById("parse-int");
+let parseIntNumber = parseInt(int.innerHTML);
+let parseIntDiv = parseIntNumber % 21;
+
+int.innerHTML.replace(parseIntNumber.value, parseIntDiv.value);
+
+// Exo 5
 let fixed = document.getElementById('to-fixed');
-let fixedValue = fixed.value;
+let fixFix = parseFloat(fixed.innerHTML);
+let fixedFix = fixFix.toFixed(2);
 
-console.log(fixedValue);
+document.getElementById('to-fixed').innerHTML = fixedFix;
 
-document.getElementById('to-fixed').innerHTML = Number.parseInt(fixed).toFixed(2) ;
